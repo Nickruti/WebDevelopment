0d12011a6afae014c0067f86979b0b5e4595778f
Nickruti <37500239+Nickruti@users.noreply.github.com>
2020-05-23 15:33:08 +0530

    Completed Login Page with alert msg

--- Django/version_1_0/login/views.py
+++ Django/version_1_0/login/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import LoginForm
 from .models import Login
 from add_sites.views import add_sites
+from django.contrib import messages
 # Create your views here.
 def login_(request):
     form = LoginForm(request.POST)
@@ -9,11 +10,13 @@ def login_(request):
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
+
             try:
                 Login.objects.get(username=username)
                 Login.objects.get(password=password)
                 
             except Login.DoesNotExist:
+                messages.error(request,'Username or Password not correct!')
                 return render(request, "login.html", {"form":form})
             return redirect(add_sites)
     else:
